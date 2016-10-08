cec843924a2580507b6c2e2c50942e160dab15a6
kouwasi <kouwasi@gmail.com>
2016-10-08 13:12:38 +0900

    push

---
+++ VACKicker/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using Rocket.API;
+
+namespace VACKicker
+{
+    public class Configuration : IRocketPluginConfiguration
+    {
+
+        public bool Enabled;
+
+        public void LoadDefaults()
+        {
+            Enabled = true;
+        }
+    }
+}
---
+++ VACKicker/VACKicker.cs
@@ -0,0 +1,52 @@
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Player;
+
+
+namespace VACKicker
+{
+    public class VACKicker : RocketPlugin<Configuration>
+    {
+        public static VACKicker Instance;
+        protected override void Load()
+        {
+            Instance = this;
+            if (Instance.Configuration.Instance.Enabled)
+            {
+                Logger.LogWarning("VACKicker: Enable");
+                U.Events.OnBeforePlayerConnected += OnBeforePlayerConnected;
+            }
+            else
+            {
+                Logger.LogWarning("VACKicker: Disable");
+            }
+            
+        }
+
+        protected override void Unload()
+        {
+
+        }
+
+        public void OnBeforePlayerConnected(UnturnedPlayer player)
+        {
+            bool? i = player.SteamProfile.IsVacBanned;
+
+            if (i == true)
+            {
+                player.Kick("VACKicker has kicked");
+                Logger.LogWarning("VACKicker -> " + player.DisplayName + " : kicked");
+            }
+            else if (i == false)
+            {
+                Logger.LogWarning("VACKicker -> " + player.DisplayName + ": No VAC record");
+            }
+            else
+            {
+                player.Kick("Oops!... VACKicker ERROR!!");
+                Logger.LogError("VACKicker -> " + player.DisplayName + ": Null");
+            }
+        }
+    }
+}
